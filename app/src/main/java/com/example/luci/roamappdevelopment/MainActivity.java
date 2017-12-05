22f0bf914035b9366ce7e80cc565a8d5617ecf0e
Sebastian <seba.basca@gmail.com>
2017-12-05 16:25:50 +0100

    update ceva 3

--- app/src/main/java/com/example/luci/roamappdevelopment/MainActivity.java
+++ app/src/main/java/com/example/luci/roamappdevelopment/MainActivity.java
@@ -80,7 +80,7 @@ import uk.co.senab.photoview.PhotoViewAttacher;
 import static android.support.v4.view.ViewPager.LayoutParams.*;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
-//pula mea e mare da da
+//PENISUL MEU ARE 500 grame!!!!
     private SectionsPagerAdapter mSectionsPagerAdapter;
     public static ViewPager mViewPager;
     EditText mySearchField;
